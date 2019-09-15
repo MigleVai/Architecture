35e6a71d9b357fea4309cda4b805aca9f710a63f
Miglė Vaitulevičiūtė <migle.vaituleviciute@mif.stud.vu.lt>
2019-09-15 21:01:33 +0300

    added functionality

---
+++ Architecture/Controllers/SaleController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Architecture.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Architecture.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Storage.SaleList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SaleModel sale)
+        {
+            sale.Id = Guid.NewGuid();
+            Storage.SaleList.Add(sale);
+            return RedirectToAction("Index", "Sale");
+        }
+
+        public IActionResult Edit(Guid id)
+        {
+            var sale = Storage.SaleList.FirstOrDefault(a => a.Id == id);
+            return View(sale);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(SaleModel sale)
+        {
+            Storage.SaleList.RemoveAll(a => a.Id == sale.Id);
+            Storage.SaleList.Add(sale);
+            return RedirectToAction("Index", "Sale");
+        }
+
+        public IActionResult Delete(Guid id) {
+            var sale = Storage.SaleList.FirstOrDefault(a => a.Id == id);
+            return View(sale);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(SaleModel sale) {
+            Storage.SaleList.RemoveAll(a => a.Id == sale.Id);
+            return RedirectToAction("Index", "Sale");
+        }
+    }
+}
---
+++ Architecture/Models/SaleModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Architecture.Models
+{
+    public class SaleModel
+    {
+        public Guid Id { get; set; }
+        public string AssetName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartSale { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndSale { get; set; }
+        [EmailAddress]
+        public string PublisherEmail { get; set; }
+    }
+}
